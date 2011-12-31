de282e3c5930758177adeab478cfe59732c07ee7
Adam Chlipala <adam@chlipala.net>
2011-12-31 15:24:27 -0500

    Tweak JavaScript to make it possible to roll back unwise Ur/Web runtime system change

--- src/js/ajaxUpload.js
+++ src/js/ajaxUpload.js
@@ -1,17 +1,51 @@
+function tweakFormCode(autoSubmit, iframeId, submitId) {
+    var subm = document.getElementById(submitId);
+    if (subm == null && thisScript != null && thisScript.parentNode != null && thisScript.parentNode.getElementsByTagName) {
+        var subms = thisScript.parentNode.getElementsByTagName("input");
+        for (var i = 0; i < subms.length; ++i)
+            if (subms[i].id == submitId) {
+                subm = subms[i];
+                break;
+            }
+    }
+    if (subm == null) er("Can't find AjaxUpload control!");
+
+    subm.parentNode.target = iframeId;
+
+    if (subm.begin == undefined) {
+        subm.begin = subm.onmousedown;
+        subm.onmousedown = undefined;
+    }
+
+    subm.parentNode.onsubmit = function() {
+        subm.begin();
+        return true;
+    };
+
+    if (subm.withHandle == undefined) {
+        subm.withHandle = subm.onkeydown;
+        subm.onkeydown = undefined;
+    }
+
+    if (autoSubmit) {
+        subm.style.visibility = "hidden";
+        for (var node = subm.previousSibling; node.tagName != "INPUT"; node = node.previousSibling);
+        node.onchange = function() { subm.parentNode.submit(); };
+    }
+}
+
 function tweakForm(autoSubmit, iframeId, submitId) {
     return "<iframe id=\""
         + iframeId
         + "\" name=\""
         + iframeId
-        + "\" src=\"#\" style=\"width:0;height:0;border:0px solid #fff;\"></iframe>\n<script type=\"text/javascript\">var subm = document.getElementById(\""
-        + submitId
-        + "\"); subm.parentNode.target = \""
+        + "\" src=\"#\" style=\"width:0;height:0;border:0px solid #fff;\"></iframe>\n<script type=\"text/javascript\">tweakFormCode("
+        + autoSubmit
+        + ",\""
         + iframeId
-        + "\"; if (subm.begin == undefined) { subm.begin = subm.onmousedown; subm.onmousedown = undefined; } subm.parentNode.onsubmit = function() { subm.begin(); return true; }; if (subm.withHandle == undefined) { subm.withHandle = subm.onkeydown; subm.onkeydown = undefined; } "
-        + (autoSubmit
-           ? "subm.style.visibility = \"hidden\"; for (var node = subm.previousSibling; node.tagName != \"INPUT\"; node = node.previousSibling); node.onchange = function() { subm.parentNode.submit(); }; "
-           : "")
-        + "</script>";
+        + "\",\""
+        + submitId
+        + "\");</script>";
 }
 
 function idToString(x) {
